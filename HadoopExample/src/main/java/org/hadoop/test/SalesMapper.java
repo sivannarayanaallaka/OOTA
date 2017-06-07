a29392765b8f676ee9535ce5d7ddfcd5bff4198f
Sivannarayana.allaka <Shiva_Naayana.Allaka@Movimentogroup.com>
2017-06-07 21:09:10 +0530

    Hadoop example

---
+++ HadoopExample/src/main/java/org/hadoop/test/SalesMapper.java
@@ -0,0 +1,22 @@
+package org.hadoop.test;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class SalesMapper extends MapReduceBase implements Mapper <LongWritable, Text, Text, IntWritable> {
+	private final static IntWritable one = new IntWritable(1);
+
+	public void map(LongWritable key, Text value, OutputCollector <Text, IntWritable> output, Reporter reporter) throws IOException {
+
+		String valueString = value.toString();
+		String[] SingleCountryData = valueString.split(",");
+		output.collect(new Text(SingleCountryData[7]), one);
+	}
+}
